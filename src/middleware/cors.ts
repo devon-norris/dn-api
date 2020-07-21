d4bec1ae6dd08cc99e42dd2e9dfed4b101b83b1b
Devon Norris <devon.norris@avoxi.com>
2020-07-21 12:54:14 -0400

    update cors for dev

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,13 +1,13 @@
 import cors from 'cors'
 import config from '../config'
-const { allowedOrigins } = config
+const { allowedOrigins, isDevelopment } = config
 
 const corsConfig: cors.CorsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
   // @ts-ignore
   origin: (origin: string, callback: Function) => {
-    if (allowedOrigins.includes(origin)) {
+    if (allowedOrigins.includes(origin) || (isDevelopment && !origin)) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
